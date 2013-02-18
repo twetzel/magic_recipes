908439ec9aad1b75d72d2b5c51a982a04439d79d
Torsten Wetzel <torstenwetzel@berlinmagic.com>
2013-02-18 15:48:26 +0100

    correct little typo

--- lib/magic_recipes/rvm.rb
+++ lib/magic_recipes/rvm.rb
@@ -22,14 +22,13 @@ module MagicRecipes
         require 'rvm/capistrano'
         
         
-        Example:
-
+        ## Example of new stuff:
         # set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
         # set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
-
         # before 'deploy:setup', 'rvm:install_rvm'   # install RVM
         # before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
         # before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+        
         
         set_default :rails_env,     'production'
         set_default :rvm_ruby,      '1.9.3'
